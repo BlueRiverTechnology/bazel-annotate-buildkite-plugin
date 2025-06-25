f367b98915aee54befcd763fa63477c4dd9cd14d
dhanush.sureshbabu <dhanush.sureshbabu@bluerivertech.com>
2025-06-24 23:21:05 -0400

    fix: optimize code
    reference: PRO-34547

--- lib/bazel-bep.bash
+++ lib/bazel-bep.bash
@@ -35,13 +35,12 @@ create_annotation() {
   if [ -n "${BUILDKITE:-}" ] && command -v buildkite-agent &>/dev/null; then
     if [ "$first" != "true" ]; then
       content=$(printf "%s" "$content" | sed '1,3d')
-      content="### 🧩 $job
+      content=$'### 🧩 '"${job_name}"$'\n\n'"${content}"
 
 $content"
     fi
     printf "%s" "$content" \
-      | buildkite-agent annotate --style "$style" --context "$context" --append
-
+      | buildkite-agent annotate --style "$style" --append
     # mark header done
     if ! buildkite-agent meta-data exists "bazel-annotate-header-created" &>/dev/null; then
       buildkite-agent meta-data set "bazel-annotate-header-created" "true" || true
